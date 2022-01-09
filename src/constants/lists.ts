51ad9bd686fa6ff6f2eaf969f825b100020eac03
duke.du <dukemecn@gmail.com>
2022-01-09 21:38:58 +0800

    1. add token gist 2. update homepage

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -17,5 +17,7 @@ export const DEFAULT_LIST_OF_LISTS: string[] = [
 //   'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json',
 //   'https://defiprime.com/defiprime.tokenlist.json',
 //   'https://umaproject.org/uma.tokenlist.json',
-     'https://gist.githubusercontent.com/dukedaily/ad8881018f36b3e508d4fd40d6777de3/raw/3bccf12ce45eda66034a101bfaa1213e9f2fb6a3/gistfile1.json'
+     'https://gist.githubusercontent.com/dukedaily/ad8881018f36b3e508d4fd40d6777de3/raw/3bccf12ce45eda66034a101bfaa1213e9f2fb6a3/gistfile1.json',
+     'https://gist.githubusercontent.com/dukedaily/6116608ba020a11c6e85f59dff34869d/raw/b5e19cc54be2d5a33c01e7feca76082455ee6364/gistfile1.json'
+
 ]
