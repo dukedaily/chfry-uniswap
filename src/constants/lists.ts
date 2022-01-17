2b8ba9e354340764e6eb13d967aec17cda378ed3
duke.du <dukemecn@gmail.com>
2022-01-17 13:46:16 +0800

    update gist.json

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -3,21 +3,20 @@ export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
-//   't2crtokens.eth', // kleros
-//   'tokens.1inch.eth', // 1inch
-//   'synths.snx.eth',
-//   'tokenlist.dharma.eth',
-//   'defi.cmc.eth',
-//   'erc20.cmc.eth',
-//   'stablecoin.cmc.eth',
-//   'tokenlist.zerion.eth',
-//   'tokenlist.aave.eth',
-//   'https://www.coingecko.com/tokens_list/uniswap/defi_100/v_0_0_0.json',
-//   'https://app.tryroll.com/tokens.json',
-//   'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json',
-//   'https://defiprime.com/defiprime.tokenlist.json',
-//   'https://umaproject.org/uma.tokenlist.json',
-     'https://gist.githubusercontent.com/dukedaily/ad8881018f36b3e508d4fd40d6777de3/raw/3bccf12ce45eda66034a101bfaa1213e9f2fb6a3/gistfile1.json',
-     'https://gist.githubusercontent.com/dukedaily/6116608ba020a11c6e85f59dff34869d/raw/b5e19cc54be2d5a33c01e7feca76082455ee6364/gistfile1.json'
+  //   't2crtokens.eth', // kleros
+  //   'tokens.1inch.eth', // 1inch
+  //   'synths.snx.eth',
+  //   'tokenlist.dharma.eth',
+  //   'defi.cmc.eth',
+  //   'erc20.cmc.eth',
+  //   'stablecoin.cmc.eth',
+  //   'tokenlist.zerion.eth',
+  //   'tokenlist.aave.eth',
+  //   'https://www.coingecko.com/tokens_list/uniswap/defi_100/v_0_0_0.json',
+  //   'https://app.tryroll.com/tokens.json',
+  //   'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json',
+  //   'https://defiprime.com/defiprime.tokenlist.json',
+  //   'https://umaproject.org/uma.tokenlist.json',
+  'https://gist.githubusercontent.com/dukedaily/6116608ba020a11c6e85f59dff34869d/raw/6453734f6ae2ab05228fe9d7ce5595b50cc41683/gistfile1.json'
 
 ]
